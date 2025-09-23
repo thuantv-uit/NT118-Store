fa9945262e8b0c7b32ee9e6d730014aa42b0422c
thuantv-uit <tvt04102004@gmail.com>
2025-09-23 17:30:41 +0700

    feat(learning): basic of the view, text, and styles

--- App.tsx
+++ App.tsx
@@ -1,15 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// JSX
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.header}>ThuanTV</Text>
+        <Text style={styles.parent}>
+          ThuanUIT 
+            <Text style={styles.child}>
+              Hello
+            </Text> 
+        </Text>
+      </View>
+      <Text style={styles.hello1}>Hello World 1 - ThuanTV</Text>
+      <Text>Hello World 2 - ThuanTV</Text>
     </View>
   );
 }
 
+// StyleSheet not CSS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +27,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello1 : { 
+    color: "green", 
+    fontSize: 30,
+    borderColor: "red",
+    borderWidth: 1,
+    padding: 10
+  },
+  header : {
+    fontSize: 20,
+    fontWeight : "bold"
+  },
+  parent: {
+    fontSize: 30,
+    color: "green"
+  },
+  child: {
+    fontSize: 25,
+    color: "violet"
+  }
 });
