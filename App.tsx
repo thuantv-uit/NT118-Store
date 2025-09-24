7dd20cadaec09f343f2f45977adc77266cc47eed
thuantv-uit <tvt04102004@gmail.com>
2025-09-24 07:59:00 +0700

    feat(learning): use TextInput

--- App.tsx
+++ App.tsx
@@ -1,13 +1,42 @@
 import { useState }from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 // JSX
 export default function App() {
   
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
 
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={{ fontSize: 20, fontWeight: "600" }}>Name: {name}</Text>
+        <TextInput
+          autoCapitalize='sentences'
+          onChangeText={(value) => setName(value)}
+          style={{
+            borderColor: "red",
+            borderWidth: 1,
+            width: 200,
+            padding: 15
+          }}
+        />
+      </View>
+      <View>
+        <Text style={{ fontSize: 20, fontWeight: "600" }}>Age: {age}</Text>
+        <TextInput
+          onChangeText={(value) => setAge(+value)}
+          style={{
+            borderColor: "red",
+            borderWidth: 1,
+            width: 200,
+            padding: 15
+          }}
+          keyboardType='numeric'
+          maxLength={2}
+        />
+      </View>
       <View>
         <Text style={{ 
           fontSize: 30, 
