5869a03ce25fdd89c922c03dbc692bc850cc83fe
thuantv-uit <tvt04102004@gmail.com>
2025-09-26 23:04:48 +0700

    feat(config): add redis to limit request of the user avoid crash app

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv"
 import { initDB } from "./config/database.js";
 import router from "./routes/transactionsRoute.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
+app.use(rateLimiter)
 app.use(express.json());
 
 const PORT = process.env.PORT;
