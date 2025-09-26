d1d2723c584c4398d11e0a0a49f9289f630f1f70
thuantv-uit <tvt04102004@gmail.com>
2025-09-26 19:48:11 +0700

    feat(config): add route get data of the user by user_id

--- Project/backend/controllers/transactionsController.js
+++ Project/backend/controllers/transactionsController.js
@@ -1,5 +1,20 @@
 import { sql } from "../config/database.js";
 
+export async function getTransactionsByUserId(req, res) {
+  try {
+    const { userId } = req.params;
+
+    const transactions = await sql`
+        SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
+      `;
+
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.log("Error getting the transactions", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
 export async function createTransaction(req, res) {
   try {
     const { title, amount, category, user_id } = req.body;
@@ -14,7 +29,8 @@ export async function createTransaction(req, res) {
       RETURNING *
     `;
 
-    console.log(transaction);
+    // To use debug
+    // console.log(transaction);
     res.status(201).json(transaction[0]);
   } catch (error) {
     console.log("Error creating the transaction", error);
--- Project/backend/routes/transactionsRoute.js
+++ Project/backend/routes/transactionsRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createTransaction } from "../controllers/transactionsController.js";
+import { 
+    createTransaction,
+    getTransactionsByUserId,
+} from "../controllers/transactionsController.js";
 
 const router = express.Router();
 
 router.post("/", createTransaction);
+router.get("/:userId", getTransactionsByUserId);
 
 export default router;
