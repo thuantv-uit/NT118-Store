37fdd092163f4699a6dc809edde1660fe820f898
thuantv-uit <tvt04102004@gmail.com>
2025-09-26 19:51:58 +0700

    feat(config): add route delete item by id

--- Project/backend/controllers/transactionsController.js
+++ Project/backend/controllers/transactionsController.js
@@ -37,3 +37,26 @@ export async function createTransaction(req, res) {
     res.status(500).json({ message: "Internal server error" });
   }
 }
+
+export async function deleteTransaction(req, res) {
+  try {
+    const { id } = req.params;
+
+    if (isNaN(parseInt(id))) {
+      return res.status(400).json({ message: "Invalid transaction ID" });
+    }
+
+    const result = await sql`
+      DELETE FROM transactions WHERE id = ${id} RETURNING *
+    `;
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+
+    res.status(200).json({ message: "Transaction deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- Project/backend/routes/transactionsRoute.js
+++ Project/backend/routes/transactionsRoute.js
@@ -2,11 +2,13 @@ import express from "express";
 import { 
     createTransaction,
     getTransactionsByUserId,
+    deleteTransaction,
 } from "../controllers/transactionsController.js";
 
 const router = express.Router();
 
 router.post("/", createTransaction);
 router.get("/:userId", getTransactionsByUserId);
+router.delete("/:id", deleteTransaction);
 
 export default router;
