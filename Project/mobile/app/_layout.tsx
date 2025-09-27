ea67816b2da7d057ab8c9ff03adc3e207412b985
thuantv-uit <tvt04102004@gmail.com>
2025-09-27 08:22:20 +0700

    feat(config): setting up styles and images

--- Project/mobile/app/_layout.tsx
+++ Project/mobile/app/_layout.tsx
@@ -1,5 +1,10 @@
+import SafeScreen from "@/components/SafeScreen";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeScreen>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeScreen>
+  );
 }
