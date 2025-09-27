7091dac65002605453f3d68df0df6801cf3ab472
thuantv-uit <tvt04102004@gmail.com>
2025-09-27 09:05:47 +0700

    feat(config): setup logic of the authentication by clerk

--- Project/mobile/app/_layout.tsx
+++ Project/mobile/app/_layout.tsx
@@ -1,10 +1,15 @@
 import SafeScreen from "@/components/SafeScreen";
-import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <SafeScreen>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeScreen>
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
   );
 }
