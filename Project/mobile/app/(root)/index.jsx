2192f71b057429900a70bc97d8c261f3f1f9c86a
thuantv-uit <tvt04102004@gmail.com>
2025-09-27 17:40:52 +0700

    feat(config): get data from backend

--- Project/mobile/app/(root)/index.jsx
+++ Project/mobile/app/(root)/index.jsx
@@ -1,15 +1,35 @@
-import { SignOutButton } from '@/components/SignOutButton'
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { SignOutButton } from '@/components/SignOutButton';
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
+import { Link } from 'expo-router';
+import { useEffect } from "react";
+import { Text, View } from 'react-native';
+import { useTransactions } from "../../hooks/useTransactions";
+
 
 export default function Page() {
   const { user } = useUser()
+
+  const { transactions, summary, isLoading, loadData, deleteTransaction } = useTransactions(
+    user.id
+  );
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  console.log("userId: ", user.id);
+
+  console.log("transactions: ", transactions)
+
+  console.log("summary: ", summary)
 
   return (
     <View>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <Text>Income: {summary.income}</Text>
+        <Text>Balance: {summary.balance}</Text>
+        <Text>Expenses: {summary.expenses}</Text>
         <SignOutButton />
       </SignedIn>
       <SignedOut>
