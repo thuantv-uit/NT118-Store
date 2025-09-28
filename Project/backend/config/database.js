90cbe355bf233eefbbf489ff9362af35f8581f08
thuantv-uit <tvt04102004@gmail.com>
2025-09-28 16:06:26 +0700

    feat(config): test workflow of the CI

--- Project/backend/config/database.js
+++ Project/backend/config/database.js
@@ -3,7 +3,7 @@ import { neon } from "@neondatabase/serverless";
 import "dotenv/config";
 
 // Creates a SQL connection using our DB URL
-export const sql = neon(procss.env.DATABASE_URL);
+export const sql = neon(process.env.DATABASE_URL);
 
 export async function initDB() {
   try {
