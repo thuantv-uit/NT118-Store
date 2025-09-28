79a1f5c4ac76dc44987328a5b6922423f2f4be8b
thuantv-uit <tvt04102004@gmail.com>
2025-09-28 16:14:55 +0700

    feat(test): test workflow of the CI

--- Project/mobile/app/(auth)/_layout.jsx
+++ Project/mobile/app/(auth)/_layout.jsx
@@ -8,5 +8,5 @@ export default function AuthRoutesLayout() {
     return <Redirect href={'/'} />
   }
 
-  return <Stack screenOptions={{headerShown: false}} />
+  return <Stack screenOptions={{headerShown: false}} /
 }
